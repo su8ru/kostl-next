ff864499f05b644107e9496ae9eb958da94aae5a
subaru <s@su8ru.dev>
2020-12-17 10:32:24 +0900

    yarn create frourio-app

---
+++ server/prisma/migrations/20201209205045_/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `label` VARCHAR(191) NOT NULL,
+    `done` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
