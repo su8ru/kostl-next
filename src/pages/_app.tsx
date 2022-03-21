6bf9fdda4219eaa092c74ecaff85b924626c86a9
subaru <s@su8ru.dev>
2022-03-22 01:35:50 +0900

    feat: allow user to scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" href={staticPath.favicon_ico} />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover,user-scalable=no"
+          content="width=device-width,initial-scale=1.0,viewport-fit=cover"
         />
       </Head>
       <ChakraProvider theme={theme}>
