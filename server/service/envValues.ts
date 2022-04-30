1b9576f30b30ec4d6a3f7338aa90dacc9a0bb622
subaru <s@su8ru.dev>
2022-05-01 01:54:16 +0900

    style: format codes

--- server/service/envValues.ts
+++ server/service/envValues.ts
@@ -2,7 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const API_SERVER_PORT = +(process.env.PORT ?? process.env.API_SERVER_PORT ?? "8080");
+const API_SERVER_PORT = +(
+  process.env.PORT ??
+  process.env.API_SERVER_PORT ??
+  "8080"
+);
 const API_BASE_PATH = process.env.API_BASE_PATH ?? "";
 const API_ORIGIN = process.env.API_ORIGIN ?? "";
 const API_ODPT_TOKEN = process.env.API_ODPT_TOKEN ?? "";
