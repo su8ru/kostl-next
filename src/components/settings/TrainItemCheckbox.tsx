e7ff0d0e543a044c82565bffda36e78acc74362d
subaru <s@su8ru.dev>
2022-05-22 16:33:35 +0900

    feat: add item setting UI

---
+++ src/components/settings/TrainItemCheckbox.tsx
@@ -0,0 +1,58 @@
+import { TrainItem } from "~/types/settings";
+import { Flex, Checkbox, Spacer, IconButton } from "@chakra-ui/react";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
+
+export type Props = {
+  itemId: TrainItem;
+  isChecked: boolean;
+  onClick: () => void;
+  moveUp?: () => void;
+  moveDown?: () => void;
+};
+
+const TrainItemCheckbox: React.VFC<Props> = ({
+  itemId,
+  isChecked,
+  onClick,
+  moveUp,
+  moveDown,
+}) => {
+  return (
+    <Flex p="1" w="48">
+      <Checkbox isChecked={isChecked} onChange={onClick}>
+        {itemName[itemId]}
+      </Checkbox>
+      <Spacer />
+      {isChecked && (
+        <>
+          <IconButton
+            size="sm"
+            variant="ghost"
+            aria-label="up"
+            icon={<BsFillCaretUpFill />}
+            onClick={moveUp}
+            isDisabled={!moveUp}
+          />
+          <IconButton
+            size="sm"
+            variant="ghost"
+            aria-label="down"
+            icon={<BsFillCaretDownFill />}
+            onClick={moveDown}
+            isDisabled={!moveDown}
+          />
+        </>
+      )}
+    </Flex>
+  );
+};
+
+export default TrainItemCheckbox;
+
+const itemName: Record<TrainItem, string> = {
+  trainId: "列車番号",
+  operationId: "運用番号",
+  destination: "行き先",
+  carId: "編成番号",
+  carCount: "編成両数",
+};
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,17 +1,26 @@
 import { NextPage } from "next";
 import { Box, Text } from "@chakra-ui/react";
 import H2 from "~/components/docs/H2";
+import H3 from "src/components/docs/H3";
 import Head from "next/head";
+import TrainItemList from "~/components/settings/TrainItemList";
 
 const Settings: NextPage = () => {
   return (
-    <Box maxW="3xl" w="100%" mx="auto" px="8" pb="8">
+    <Box maxW="3xl" w="100%" mx="auto" px="8" pb="8" textAlign="left">
       <Head>
         <title>設定 - こすとれ</title>
       </Head>
       <Box as="section" mt="12">
         <H2>設定</H2>
-        <Text mt="4">準備中…</Text>
+        <H3>表示項目</H3>
+        <Text mt="4">
+          列車アイコンに表示する項目を設定できます。チェックボックスで表示の有無を、右の上下ボタンで表示順序を設定できます。
+        </Text>
+        <Text mt="4">「行き先」は一番下にするのがおすすめです。</Text>
+        <Box mt="4">
+          <TrainItemList />
+        </Box>
       </Box>
     </Box>
   );
