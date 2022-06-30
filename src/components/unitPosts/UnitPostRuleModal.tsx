57006939f55f762646624e1d7b85a6477f8e269f
subaru <s@su8ru.dev>
2022-07-01 03:27:58 +0900

    fix: set bg color of pre

--- src/components/unitPosts/UnitPostRuleModal.tsx
+++ src/components/unitPosts/UnitPostRuleModal.tsx
@@ -9,7 +9,10 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
+  Theme,
   UnorderedList,
+  useColorModeValue,
+  useTheme,
 } from "@chakra-ui/react";
 
 export type Props = {
@@ -17,38 +20,46 @@ export type Props = {
   onClose: () => void;
 };
 
-const UnitPostRuleModal: React.VFC<Props> = ({ isOpen, onClose }) => (
-  <Modal isOpen={isOpen} onClose={onClose} isCentered>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>編成運用投稿 入力ルール</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <Box as="pre" bgColor="gray.100" borderRadius="base" px="4" py="2">
-          {`No.12 8710F
+const UnitPostRuleModal: React.VFC<Props> = ({ isOpen, onClose }) => {
+  const theme = useTheme<Theme>();
+  const preBgColor = useColorModeValue(
+    theme.colors.gray["100"],
+    theme.colors.gray["600"]
+  );
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>編成運用投稿 入力ルール</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box as="pre" bgColor={preBgColor} borderRadius="base" px="4" py="2">
+            {`No.12 8710F
 82 8702F
 25T 600F
 14 7805F+7705F
 51K 5736F`}
-        </Box>
-        <UnorderedList spacing="1" my="2" fontSize="sm">
-          <ListItem>
-            運用番号 編成番号 の順に改行区切りで入力してください。
-          </ListItem>
-          <ListItem>No. は省略可能です。</ListItem>
-          <ListItem>都営車は下 3 桁のみ入力してください。</ListItem>
-          <ListItem>
-            連結している場合は、新宿側から順に + で繋げて入力してください。
-          </ListItem>
-        </UnorderedList>
-        <Divider />
-        <Text fontSize="sm" my="2">
-          入力された情報は直ちに全てのユーザーへ反映されます。情報の正確性に留意してください。悪質な入力には
-          BAN を含めた厳正な対応を行います。
-        </Text>
-      </ModalBody>
-    </ModalContent>
-  </Modal>
-);
+          </Box>
+          <UnorderedList spacing="1" my="2" fontSize="sm">
+            <ListItem>
+              運用番号 編成番号 の順に改行区切りで入力してください。
+            </ListItem>
+            <ListItem>No. は省略可能です。</ListItem>
+            <ListItem>都営車は下 3 桁のみ入力してください。</ListItem>
+            <ListItem>
+              連結している場合は、新宿側から順に + で繋げて入力してください。
+            </ListItem>
+          </UnorderedList>
+          <Divider />
+          <Text fontSize="sm" my="2">
+            入力された情報は直ちに全てのユーザーへ反映されます。情報の正確性に留意してください。悪質な入力には
+            BAN を含めた厳正な対応を行います。
+          </Text>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default UnitPostRuleModal;
