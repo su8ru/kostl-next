d75e341da3ea629045b5fa9c71f8f8d3a7181914
subaru <s@su8ru.dev>
2022-07-16 05:48:49 +0900

    fix: set viewport-fit to meta viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,viewport-fit=cover"
         />
       </Head>
       <ChakraProvider theme={theme}>
